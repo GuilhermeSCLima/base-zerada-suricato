698816110b75ead9e3917f4adad7285dd8e40e3d
Guilherme Lima <joao_guilherme0511@outlook.com>
2022-06-02 16:29:36 -0300

    Finalização modulo 5

---
+++ resources/[curso]/[Anotações]/prefixos.lua
@@ -0,0 +1,21 @@
+--! Prefixos
+
+-- * Set (setar / aplicar)
+
+-- SetEntityCoords()
+-- SetEntityHealth()
+-- SetEntityAlpha()
+
+-- * Is (é / esta) (retorno de um dado)
+
+  -- IsPlayerDead()      -- retorna true ou false
+  -- IsEntityAPed()      -- retorna true ou false
+  -- IsPlayerClimbing()  -- retorna true ou false
+  -- IsPedJumping()      -- retorna true ou false
+
+-- * Get() (retorno de dado)
+
+  -- GetPlayerName()     -- retorna o nome do jogador
+  -- GetPlayerPed()      -- retorna o ped do jogador
+  -- GetPlayerCoords()   -- retorna as coordenadas do jogador
+  -- GetPlayerHealth()   -- retorna a vida do jogador
---
+++ resources/[curso]/[Anotações]/server-client.lua
@@ -0,0 +1,26 @@
+-- Existem nativas que so existem em um lado, seja client ou server
+--[[ 
+  Servidor = quem está hosteando
+  Cliente = quem está conectado ou se conectando
+]]
+
+--[[ 
+  Primeira coisa a se pensar é, se a ação será realizada na maquina do jogador ou na maquina onde o servidore está.
+]]
+
+--! Exemplos
+
+--[[ 
+  --? Client
+
+  Criar um carro
+  Fazer o player fazer uma animação
+  ficar invisivel
+  teleportar um jogador
+
+  --? Server
+  Kickar um jogador
+  desligar um script
+  ligar um script
+
+]]
